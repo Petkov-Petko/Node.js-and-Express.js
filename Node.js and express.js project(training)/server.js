b6240155b3896cb9a873b2ae7fee8a09df75ddfb
Petko Petkov <petkozlatilov1234@gmail.com>
2024-02-18 17:03:46 +0200

    Created server

---
+++ Node.js and express.js project(training)/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+// dotenv.config(); // I get error??????
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
