4c516c467f33feaacf3ae270f31505c395cadfb4
Petko Petkov <petkozlatilov1234@gmail.com>
2024-02-21 21:26:16 +0200

    object with all needed errors

--- Node.js and express.js project(training)/middleware/errorHandler.js
+++ Node.js and express.js project(training)/middleware/errorHandler.js
@@ -1,14 +1,39 @@
-export const errorHandler = ( err, req, res, next ) => {
-    const statusCode = res.statusCode ? res.statusCode : 5000;
+import { constants } from "../constants.js";
 
-    switch (statusCode) {
-        case 400:
-            res.json({title: "Validation Failed", message: err.message, stackTrace: err.stack});
-            break;
-        case 404:
-            res.json({title: "Not Found", message: err.message, stackTrace: err.stack});
-            break
-        default:
-            break;
-    }
+export const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 5000;
+
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: "Validation Failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.NOT_FOUND:
+      res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.FORBIDDEN:
+      res.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    default:
+        console.log("No errors.");
+      break;
+  }
 };
